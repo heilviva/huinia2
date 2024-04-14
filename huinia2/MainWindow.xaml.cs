f491484af8b1cfae0b6dcb00912755a03a54b533
heilviva <ep77777777@yandex.ru>
2024-04-15 02:38:54 +0300

    Добавьте файлы проекта.

---
+++ huinia2/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace huinia2
+{
+    public partial class MainWindow : Window
+    {
+        private ITCompanyEntities context = new ITCompanyEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void forFirstGrid_Click(object sender, RoutedEventArgs e)
+        {
+            first_frame.Content = new FirstGrid();
+        }
+
+        private void forSecondGrid_Click(object sender, RoutedEventArgs e)
+        {
+            second_frame.Content = new SecondGrid();
+        }
+
+        private void forThirdGrid_Click(object sender, RoutedEventArgs e)
+        {
+            third_frame.Content = new ThirdGrid();
+        }
+    }
+}
